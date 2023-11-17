a46eff10e16f7e16f7e1a27ce36e9c11d571aeaa
Richard Sneyd <richardsneyd@hotmail.com>
2023-11-17 16:48:13 +0000

    further refinements to Actor and Species

--- src/entities/Actor.ts
+++ src/entities/Actor.ts
@@ -2,19 +2,20 @@ import Entity from "./Entity";
 import { IPosition } from "./IPosition";
 import Item from "./Item";
 import Profile from "./Profile";
+import Species, { ISpecies } from "./Species";
 
 type Mood = "very sad" | "sad" | "happy" | "very happy" | "excited" | "scared" | "confused" | "bored" | "curious" | "tired" | "sorry" | "angry" | "lonely" | null;
 type Gender = "male" | "female" | null;
 
 class Actor extends Entity {
-    private _species: string;
+    private _species: ISpecies;
     private _mood: Mood;
     private _gender: Gender;
     private _age: number;
     private _profile: Profile;
     private _items: Item[];
 
-    constructor({name, position, mood = "happy", species = "human", gender = "male", age = 9, profile = new Profile({}), items = []}:{name: string, position: IPosition, mood: Mood, species: string, gender: Gender, age: number, profile: Profile, items: Item[]}) {
+    constructor({name, position, mood = "happy", gender = "male", age = 9, profile = new Profile({}), items = [], species = new Species("dog")}:{name: string, position: IPosition, mood: Mood, species: ISpecies, gender: Gender, age: number, profile: Profile, items: Item[]}) {
         super(name, position);
         this._mood = mood;
         this._species = species;
@@ -24,7 +25,7 @@ class Actor extends Entity {
         this._items = items;
     }
 
-    get species(): string {
+    get species(): ISpecies {
         return this._species;
     }
 
---
+++ src/entities/Species.ts
@@ -0,0 +1,35 @@
+interface ISpecies {
+    get base(): string;
+    get plural(): string;
+}
+
+class Species implements ISpecies {
+    private _base: string;
+    private _plural: string;
+
+    constructor(base: string) {
+        this._base = base;
+        this._plural = this._determinePluralForm(base);
+    }
+
+    get base(): string {
+        return this._base;
+    }
+
+    get plural(): string {
+        return this._plural;
+    }
+
+    private _determinePluralForm(base: string): string {
+        const esEndings = ['s', 'ss', 'sh', 'ch', 'x', 'z'];
+        for (const ending of esEndings) {
+            if (base.endsWith(ending)) {
+                return base + 'es';
+            }
+        }
+        return base + 's';
+    }
+}
+
+export default Species;
+export type {ISpecies, Species}
