1400cc2b27b4989ab5525dd69a0f9dc2dc4ea3d3
Richard Sneyd <richardsneyd@hotmail.com>
2023-12-05 16:08:18 +0000

    fixes mocking in StoryFactory test file, adds case

--- src/stories/StoryFactory.test.ts
+++ src/stories/StoryFactory.test.ts
@@ -2,30 +2,37 @@ import StoryFactory from './StoryFactory';
 import { IStory } from "./IStory";
 import IBiome from '../biome/IBiome';
 import Biome from '../biome/Biome';
-import Location from '../scenes/Location';
 import { IActor } from '../entities/IActor';
 import Actor from '../entities/Actor';
 import Scene from '../scenes/Scene';
+import Location from '../scenes/Location';
 jest.mock('../scenes/Scene');
 jest.mock('../entities/Actor');
 jest.mock('../scenes/Location');
 
-describe('StoryFactory', () => {
-    let location1: Location;
-    let location2: Location;
-    let location3: Location;
-    let location4: Location;
+jest.mock('../biome/Biome', () => {
+    return {
+        __esModule: true,
+        default: jest.fn().mockImplementation(() => {
+            return {
+                locations: new Set([new (Location as any)(), new (Location as any)()]),
+                actors: new Set([new (Actor as any)(), new (Actor as any)()]),
+                items: new Set([new (Actor as any)(), new (Actor as any)()]),
+                selectRandomActor: jest.fn().mockReturnValue(new (Actor as any)()),
+                selectRandomItem: jest.fn().mockReturnValue(new (Actor as any)())
+            }
+        })
+    }
+});
 
+
+describe('StoryFactory', () => {
     let protagonist: IActor;
     let biome: IBiome;
 
     beforeEach(() => {
-        location1 = new (Location as any)();
-        location2 = new (Location as any)();
-        location3 = new (Location as any)();
-        location4 = new (Location as any)();
         protagonist = new (Actor as any)();
-        biome = new Biome({ locations: [location1, location2, location3, location4] });
+        biome = new (Biome as any)();
     });
 
     afterEach(() => {
@@ -37,16 +44,21 @@ describe('StoryFactory', () => {
         expect(story.scenes.length).toEqual(0);
     });
 
+    test('creates story with no protagonist, chosen from biome at random', () => {
+        const story: IStory = StoryFactory.new({ biome });
+        expect(story.protagonist).toBeInstanceOf(Actor);
+    })
+
     test('creates story with a single scene', () => {
-        const scene = new Scene({ location: location1 });
+        const scene = new (Scene as any)();
         const story: IStory = StoryFactory.new({ biome, protagonist: protagonist });
         story.scenes.push(scene);
         expect(story.scenes.length).toEqual(1);
     });
 
     test('creates story with multiple scenes', () => {
-        const scene1 = new Scene({ location: location1 });
-        const scene2 = new Scene({ location: location2 });
+        const scene1 = new (Scene as any)();
+        const scene2 = new (Scene as any)();
         const story: IStory = StoryFactory.new({ biome, protagonist: protagonist });
         story.scenes.push(scene1, scene2);
         expect(story.scenes.length).toEqual(2);
